a6fa66c6a643eed7f2a65b38b9000d23d9843c13
Akash Vishwakarma <skytup91@gmail.com>
2026-01-18 03:48:28 +0530

    design: Add browser Router and setup different routes for blog

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,44 @@ import { BlogDetail } from "./components/BlogDetail"
 import './App.css'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-// import { BrowserRouter, Route, Link, Routes } from "react-router-dom"
+import Footer from "./components/Footer"
+import CreateBlogForm from "./components/CreateBlogForm"
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom"
+
+// const [modal, ShowModal] = useState(true);
+
 
 function App() {
   const [selectedId, setSelectedId] = useState<number>(0)
+  function Home() {
+    return (
+      <>
+        <Hero />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+          <div>
+            <BlogList onSelect={setSelectedId} />
+          </div>
+          <div className="md:col-span-2">
+            <BlogDetail blogId={selectedId} />
+          </div>
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
-      <Navbar />
-      <Hero />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
-        <div>
-          <BlogList onSelect={setSelectedId} />
-        </div>
-        <div className="md:col-span-2">
-          <BlogDetail blogId={selectedId} />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes >
+          <Route path="/create-blog" element={<CreateBlogForm />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          {/* <CreateBlogForm /> */}
+        </Routes>
+        <Footer />
+
+      </BrowserRouter>
+
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 import { Button } from "@/components/ui/button"
-
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-6 py-4 border-b">
       <h1 className="text-m font-semibold">Camonk Blog</h1>
 
       <div className="space-x-4">
-        <Button>Home</Button>
-        <Button>Blogs</Button>
-        <Button>New Blog</Button>
+        <Link to={'/'}><Button> Home </Button></Link>
+        <Link to={'/create-blog'}><Button> Create Blog </Button></Link>
+        <Link to={'/blog'}><Button> BLog </Button></Link>
       </div>
     </nav>
   )
